5ca498bce3c28f6d3bd53bfe65ed07b0f5e3575b
jumanah <jumanah.f.14@gmail.com>
2023-08-10 01:03:08 +0300

    add

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // / javascript
-
-let name=jumanah
-let age=18
-
- // // test 
-// // const name;
-// // name=jojo
-// const age;
-// age=18
-<PublicKeyCredential;fkvdfm
-
-
+//Number To String
+let num=42;
+const numTOstr=String(num);
+console.log(`${typeof num} ${num}`);
+console.log(`${typeof numTOstr} ${numTOstr}`);
+//String To Number
+let str="25";
+const strTOnum=Number(str);
+console.log(`${typeof str} ${str}`);
+console.log(`${typeof strTOnum} ${strTOnum}`);
+////////////
+let str1="10";
+let str1Tonum=Number(str1);
+let math= (str1)-30;
+let math2= (str1)+2;
+console.log(`${typeof str1Tonum}:${str1Tonum}`);
+//////////////
+let str2="9";
+let num1=5;
+console.log(str2+num1);
+console.log(str2-num1);
+////////////////
+let boolean=true;
+let num2=9;
+let booleanTonum=Number(boolean);
+console.log(`${typeof booleanTonum}:${booleanTonum}`);
+///////////////////
