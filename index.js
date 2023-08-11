1749012f137cc9f5a3f539aca2923a48f61d2c88
jumanah <jumanah.f.14@gmail.com>
2023-08-11 15:31:31 +0300

    add

--- index.js
+++ index.js
@@ -1,28 +1,200 @@
 // / javascript
-//Number To String
-let num=42;
-const numTOstr=String(num);
-console.log(`${typeof num} ${num}`);
-console.log(`${typeof numTOstr} ${numTOstr}`);
-//String To Number
-let str="25";
-const strTOnum=Number(str);
-console.log(`${typeof str} ${str}`);
-console.log(`${typeof strTOnum} ${strTOnum}`);
-////////////
-let str1="10";
-let str1Tonum=Number(str1);
-let math= (str1)-30;
-let math2= (str1)+2;
-console.log(`${typeof str1Tonum}:${str1Tonum}`);
-//////////////
-let str2="9";
-let num1=5;
-console.log(str2+num1);
-console.log(str2-num1);
-////////////////
-let boolean=true;
-let num2=9;
-let booleanTonum=Number(boolean);
-console.log(`${typeof booleanTonum}:${booleanTonum}`);
-///////////////////
+// project 1 >>>>>>>
+//   // Boolean
+
+// let test1=false;
+// let test2=true;
+// console.log(test1);
+// console.log(test2);
+// console.log(typeof test1);
+
+//   // Symbol
+// let id=Symbol("id")
+// console.log(id);
+// console.log(typeof id);
+
+// // unll type
+// let test3=null;
+// console.log(test3);
+// console.log( typeof test3);
+
+// // undefined
+// let test5;
+// console.log(test5);
+// console.log( typeof test5);
+
+// // integer
+// let tuiwq1=2.45;
+// console.log(tuiwq1);
+// console.log(typeof tuiwq1)
+
+// // bigint
+// let tuiwq2=123455677n
+// console.log(tuiwq2);
+// console.log( typeof tuiwq2);
+
+// let x0=20n-10n;
+// console.log(x0)
+
+// // string type
+// let text="Hello form javascript Bootcamp";
+// let text2='Hello form javascript Bootcamp';
+// let text3="Hello form javascript Bootcamp";
+// let text4="Hello form 'javascript'Bootcamp";
+// console.log(text3);
+// console.log(text2);
+// console.log(text2);
+// console.log(text);
+// console.log(text4);
+// // string 
+// let messag = "test"- "test"
+// console.log(messag);
+// console.log( typeof messag);
+
+// let messag2="30"+"10"
+// console.log(messag2);
+// console.log( typeof messag2);
+
+// // interpolation
+// let country="Riyadh"
+// let continent="Asia"
+// // console.log(sentence);
+
+// // String propreti
+// let str="java script language"
+// console.log(str.length);
+// console.log(str.charCodeAt(0));
+// console.log(str.split(" ",3));
+
+
+
+
+
+
+
+
+// project 2>>>>>>>
+
+// let str1=String();
+// let num=Number();
+// let bool=Boolean();
+
+// String to string
+// const str="text";
+// const strTostr=String(str)
+// console.log(`${typeof str}: ${str}`);
+// console.log(`${typeof strTostr}: ${strTostr}`);
+
+// // num TO string
+// const num_1=20;
+// const numToStr=String(num_1)
+// console.log(`${typeof num_1} :${num_1}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+
+// // boolean To String
+// const bool_1=true;
+// const boolTostr=String(bool_1)
+// console.log(`${typeof bool_1}:${bool_1}`);
+// console.log(`${typeof boolTostr}:${boolTostr}`);
+
+// // undefined To string
+
+// const Un=undefined;
+// const UnTostr=String(Un)
+// console.log(`${typeof Un}:${Un}`);
+// console.log(`${typeof UnTostr}:${UnTostr}`);
+
+// // Null To string
+
+// const Null1=null;
+// const NullTostr=String(null)
+// console.log(`${typeof null}:${null}`);
+// console.log(`${typeof NullTostr}:${NullTostr}`);
+
+// // Bigint To string
+
+// const Bigint_2=2222222222222n;
+// const BigintTostr=String(Bigint_2)
+// console.log(`${typeof Bigint_2}:${Bigint_2}`);
+// console.log(`${typeof BigintTostr}:${BigintTostr}`);
+// // Number to Number
+
+// const N_um=4;
+// const NumToStr2=Number(N_um)
+// console.log(`${typeof N_um}:${N_um}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+// // string to Number ("11")
+
+// const N_um2="11";
+// const NumToStr=Number(N_um2)
+// console.log(`${typeof N_um2}:${N_um2}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+
+// // string To Number("text")
+
+// const N_um3=44
+// const NumToStr4=Number(N_um3)
+// console.log(`${typeof N_um3}:${N_um3}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+// // bigint to number
+
+// const N_um4=""
+// const NumToStr5=Number(N_um)
+// console.log(`${typeof N_um3}:${N_um3}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+// // undefined to number
+
+// const N_um6=""
+// const NumToStr8=Number(N_um)
+// console.log(`${typeof N_um3}:${N_um3}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+// // boolean to number
+
+// const N_um7=""
+// const NumToStr7=Number(N_um)
+// console.log(`${typeof N_um3}:${N_um3}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+// // null to number
+
+// const N_um9=""
+// const NumToStr9=Number(N_um)
+// console.log(`${typeof N_um3}:${N_um3}`);
+// console.log(`${typeof numToStr}:${numToStr}`);
+// // Conversion to Boolean
+
+// const bool_11=true;
+// const boolToBool=Boolean(bool_11)
+// console.log(`${typeof bool_11}:${bool_11}`);
+// console.log(`${typeof boolToBool}:${boolToBool}`);
+// // Num To Boolean (0)>>false
+
+// const Bool_12=0;
+// const boolToBool2=Boolean(Bool_12)
+// console.log(`${typeof Bool_12}:${Bool_12}`);
+// console.log(`${typeof boolToBool}:${boolToBool}`);
+// // Num To Boolean (66)
+
+// const Bool_13=66;
+// const boolToBool3=Boolean(Bool_13)
+// console.log(`${typeof Bool_13}:${Bool_13}`);
+// console.log(`${typeof boolToBool}:${boolToBool}`);
+// // undefined to Boolean >> false
+// const Un1=undefined;
+// const UnTostr1=Boolean(Un1)
+// console.log(`${typeof Un1}:${Un1}`);
+// console.log(`${typeof UnTostr}:${UnTostr}`);
+// // Null to Boolean >> false
+// const Null12=null;
+// const UnTostr2=Boolean(Null12)
+// console.log(`${typeof Null12}:${Null12}`);
+// console.log(`${typeof UnTostr}:${UnTostr}`);
+// // String to Boolean
+// const String1="text";
+// const  strTostr3=Boolean( String1)
+// console.log(`${typeof  String1}:${ String1}`);
+// console.log(`${typeof UnTostr}:${UnTostr}`);
+// // empty to Boolean ("")>>false
+// const empty_1="";
+// const tobool=Boolean(empty_1)
+// console.log(`${empty_1}`);
+// console.log(`${tobool}`);
